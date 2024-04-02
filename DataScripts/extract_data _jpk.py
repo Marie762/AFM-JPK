ba67e243fca080af10b8a3b9c452b3fff6b21fc5
Marie <m.a.matzdorf@student.tudelft.nl>
2024-04-02 14:41:13 +0200

    extracting from jpk-force files is working

--- DataScripts/extract_data _jpk.py
+++ DataScripts/extract_data _jpk.py
@@ -21,40 +21,51 @@ sns.set_theme(style="whitegrid", palette="muted")
 	# dslist[0]["force"]
 
 
-# to be able to load all files from a folder
+# List all jpk-force files from the folder 'Data'
 allfilesinfolder = os.listdir(r'Data') 
 must_end_in = '.jpk-force'
 jpk_force_files = [os.path.join('Data',file) for file in allfilesinfolder if file[-len(must_end_in):] == must_end_in]
 
-# create empty list
+# create empty list to store all the data extracted from each jpk-force file
 jpk_force_data_list = []
-# for loop to add the all the separate data to a list with size equal to the number of files
+# for loop to extract and append all the separate jpk-force data to the list jpk_force_data_list (length equal to the number of files in folder 'Data')
 for i in range(len(jpk_force_files)):
-	data = afmformats.load_data(jpk_force_files[i])[0] # format of a dictionary
-	jpk_force_data_list.append(data) # add the dictionary to a list
+	data_extract = afmformats.load_data(jpk_force_files[i])[0] 
+	jpk_force_data_list.append(data_extract) 
+	print(jpk_force_data_list)
 
-# to access the dictionaries in the list: list[0][0]["force"]
+# to access specific data in jpk_force_data_list: 
+	# jpk_force_data_list[0].columns - returns the column names of file 1 (corresponds to element 0)
+	# jpk_force_data_list[0]["force"] - returns an array with the force data of file 1 (corresponds to element 0)
 
 
 # scale conversion constants
-xsc = 1 # none
 ysc = 1e9 # nN
 dsc = 1e6 # microns
 
+# create three empty lists to store the height (d), force (F), and time (t) values of each jpk-force file
 d = []
 F = []
+t = []
 
-for j in range(len(allfilesinfolder)):
-	
+# add all the height, force, and time data to separate lists, with the element corresponding to the jpk_force_data_list
+for j in range(len(jpk_force_files)):
 	d.append(jpk_force_data_list[j]["height (measured)"]*dsc)
 	F.append(jpk_force_data_list[j]["force"]*ysc)
-	
-for k in range(len(allfilesinfolder)):
-	fig, ax = plt.subplots()
-	ax.plot(d[k], F[k])
+	t.append(jpk_force_data_list[j]["time"])
 
-	ax.set(xlabel='height measured (um)', ylabel='force (nN)',
-       title='Force-distance curve %i' % k)
+# Plotting force-distance curve and saving a png to the 'Results' folder
+for k in range(len(jpk_force_files)):
+    # create figure 'ax' and plot F against d (force-distance curve)
+    fig, ax = plt.subplots()
+    ax.plot(d[k], F[k])
+    ax.set(xlabel='height measured (um)', ylabel='force (nN)', title='Force-distance curve %i' % k)
+    fig.savefig('Results\Fd_' + str(k) + '.png')
 
-	fig.savefig('Results\Fd_' + str(k) + '.png')
-	# plt.show()
+# Plotting force-time curve and saving a png to the 'Results' folder
+for k in range(len(jpk_force_files)):
+    # create figure 'ax' and plot F against d (force-distance curve)
+    fig, ax = plt.subplots()
+    ax.plot(d[k], F[k])
+    ax.set(xlabel='time (s)', ylabel='force (nN)', title='Force-time curve %i' % k)
+    fig.savefig('Results\Ft_' + str(k) + '.png')
