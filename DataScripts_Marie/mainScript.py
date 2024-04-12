efd19f8daf511476e8c722e261f464a46782e446
Marie <m.a.matzdorf@student.tudelft.nl>
2024-04-12 15:41:15 +0200

    working on data smoothing

--- DataScripts_Marie/mainScript.py
+++ DataScripts_Marie/mainScript.py
@@ -20,18 +20,18 @@ d, F, t = extractJPK.force()
 # test plotting
 #fig = plot.Fd(F, d)
 #fig = plot.Ft(F, t)
-
+#plt.show()
 
 # test basic processing
-max_value, max_element = procBasic.max(F)
+#max_value, max_element = procBasic.max(F)
 
 F_bS = procBasic.baselineSubtraction(F)
 #fig = plot.Fdsubplot(F, d, F_bS, subplot_name='baseline subtraction')
 #fig = plot.Ftsubplot(F, t, F_bS, subplot_name='baseline subtraction')
-plt.show()
+#plt.show()
 
 
-window_size = 10
+window_size = 200
 poly_order = 2
 F_smoothSG = procBasic.smoothingSG(F_bS, window_size, poly_order)
 
--- DataScripts_Marie/procBasic.py
+++ DataScripts_Marie/procBasic.py
@@ -29,44 +29,44 @@ def baselineSubtraction(F):
     return F_bS
 
 def smoothingSG(F, window_size, poly_order):
-    F_smoothSG= []
+    F_smoothSG = []
     for k in range(len(F)):
         F_smoothSG_local = []
         if len(F[k][0]) > window_size:
             smoothed_data1 = savgol_filter(F[k][0], window_size, poly_order) # smoothing Savitzky-Golay filter
         else:
-            window_size = len(F[k][0])
-            if window_size > poly_order:
-                smoothed_data1 = savgol_filter(F[k][0], window_size, poly_order)
+            window_size1 = len(F[k][0])
+            if window_size1 > poly_order:
+                smoothed_data1 = savgol_filter(F[k][0], window_size1, poly_order)
             else:
-                poly_order = window_size - 1
-                smoothed_data1 = savgol_filter(F[k][0], window_size, poly_order)
+                poly_order1 = window_size1 - 1
+                smoothed_data1 = savgol_filter(F[k][0], window_size1, poly_order1)
         F_smoothSG_local.append(smoothed_data1)
-        
+
         if len(F[k][1]) > window_size:
             smoothed_data2 = savgol_filter(F[k][1], window_size, poly_order)
         else:
-            window_size = len(F[k][1])
-            if window_size > poly_order:
-                smoothed_data2 = savgol_filter(F[k][1], window_size, poly_order)
+            window_size2 = len(F[k][1])
+            if window_size2 > poly_order:
+                smoothed_data2 = savgol_filter(F[k][1], window_size2, poly_order)
             else:
-                poly_order = window_size - 1
-                smoothed_data2 = savgol_filter(F[k][1], window_size, poly_order)
+                poly_order2 = window_size2 - 1
+                smoothed_data2 = savgol_filter(F[k][1], window_size2, poly_order2)
         F_smoothSG_local.append(smoothed_data2)
-        
+
         if len(F[k]) > 2:
             if len(F[k][2]) > window_size:
                 smoothed_data3 = savgol_filter(F[k][2], window_size, poly_order)
             else:
-                window_size = len(F[k][2])
-                if window_size > poly_order:
-                    smoothed_data3 = savgol_filter(F[k][2], window_size, poly_order)
+                window_size3 = len(F[k][2])
+                if window_size3 > poly_order:
+                    smoothed_data3 = savgol_filter(F[k][2], window_size3, poly_order)
                 else:
-                    poly_order = window_size - 1
-                    smoothed_data3 = savgol_filter(F[k][2], window_size, poly_order)
+                    poly_order3 = window_size3 - 1
+                    smoothed_data3 = savgol_filter(F[k][2], window_size3, poly_order3)
             F_smoothSG_local.append(smoothed_data3)
 
-        F_smoothSG.append(F_smoothSG)
+        F_smoothSG.append(F_smoothSG_local)
         
     return F_smoothSG
 
