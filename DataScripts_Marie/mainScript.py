b0be71f4fea7081b1219c504cf99e631c1312589
Marie <m.a.matzdorf@student.tudelft.nl>
2024-04-15 11:37:32 +0200

    none

--- DataScripts_Marie/mainScript.py
+++ DataScripts_Marie/mainScript.py
@@ -14,8 +14,23 @@ import metadata
 # extract the force spectroscopy data from all the jpk-force files in the directory 'Data'
 d, F, t = extractJPK.force()
 spring_constant_list = metadata.SpringConstant()
-print(spring_constant_list)
 
 # extract the QI data from all the jpk-qi-data files in the directory 'Data_QI'
 #qmap = extractJPK.QI()
 
+force_arr = F[3][0]
+
+k = spring_constant_list[3]
+
+deflection_arr = force_arr/k
+
+z = d[3][0]
+
+delta = z - deflection_arr
+
+fig, ax = plt.subplots()
+ax.plot(d[3][0], F[3][0], 'deepskyblue')
+ax.plot(delta, F[3][0], 'orange')
+ax.set(xlabel='height measured (um)', ylabel='force (nN)', title='Force-distance curve %i' % k)
+
+plt.show()
