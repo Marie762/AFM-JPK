da4cadaedfe544b2b76efa8eb24c02c920dd287f
Marie <m.a.matzdorf@student.tudelft.nl>
2024-04-17 11:33:12 +0200

    contact point estimation is working

--- DataScripts_Marie/mainScript.py
+++ DataScripts_Marie/mainScript.py
@@ -10,47 +10,19 @@ import extractJPK
 import plot
 import procBasic
 import metadata
+from scipy.signal import argrelmin
 
 # extract the QI data from all the jpk-qi-data files in the directory 'Data_QI'
 #qmap = extractJPK.QI()
 
 # extract the force spectroscopy data from all the jpk-force files in the directory 'Data'
 d, F, t = extractJPK.force()
-spring_constant_list = metadata.SpringConstant()
-F_bS = procBasic.baselineSubtraction(F)
+#spring_constant_list = metadata.SpringConstant()
 
-#compute first 10% of data set
-i=0
-percentage = 10
-decimal = percentage/100
-length = len(F_bS[i][0])
-percentage_of_length = length*decimal
-round_percentage_of_length = round(percentage_of_length)
-
-# linear fit of first 10% of dataset
-m,b = np.polyfit(d[i][0][:round_percentage_of_length], F_bS[i][0][:round_percentage_of_length], 1)
-
-print(m)
-print(b)
-x = d[i][0]
-lin_fit = m*x + b
-fig, ax = plt.subplots()
-ax.plot(x, F_bS[i][0], 'deepskyblue')
-ax.plot(x, lin_fit, 'orange')
-ax.set(xlabel='d', ylabel='force', title='Force-distance curve %i' % i)
-
+M, B = procBasic.baselineLinearFit(F, d, percentage=50, plot='True', saveplot='True')
+argmin_list = procBasic.contactPoint(F,d, plot='True', saveplot='True')
+print(argmin_list)
 plt.show()
-
-# fig, ax = plt.subplots()
-# ax.plot(d[7][0], F[7][0], 'purple')
-# ax.plot(d[6][0], F[6][0], 'g')
-# ax.plot(d[5][0], F[5][0], 'y')
-# ax.plot(d[4][0], F_bS[4][0], 'm')
-# ax.plot(d[3][0], F_bS[3][0], 'deepskyblue')
-# ax.plot(d[2][0], F_bS[2][0], 'orange')
-# ax.plot(d[1][0], F_bS[1][0], 'r')
-# ax.plot(d[0][0], F_bS[0][0], 'b')
-# ax.set(xlabel='height measured (um)', ylabel='force (nN)', title='Force-distance curve %i' % k)
 
 # fig, ax = plt.subplots()
 # ax.plot(z, f, 'deepskyblue')
--- DataScripts_Marie/procBasic.py
+++ DataScripts_Marie/procBasic.py
@@ -6,6 +6,7 @@ Created on Tue Apr 3 2024
 """
 from scipy.signal import savgol_filter
 import numpy as np
+import matplotlib.pylab as plt
 
 
 def max(F):
@@ -70,3 +71,53 @@ def smoothingSG(F, window_size, poly_order):
         
     return F_smoothSG
 
+def baselineLinearFit(F, d, percentage=50, plot='False', saveplot='False'):
+    # two empty lists to store the gradients 'm' and constants 'b' of the linear fit function for each curve
+    M = []
+    B = []
+    for i in range(len(F)):
+        percentage_slice = round((percentage/100)*len(F[i][0])) #compute first ..% of data set and round to nearest integer
+        m,b = np.polyfit(d[i][0][:percentage_slice], F[i][0][:percentage_slice], 1) # linear fit of first ..% of dataset
+        M.append(m) # store in lists
+        B.append(b)
+        
+        if plot == 'True':
+            x = d[i][0]
+            lin_fit = m*x + b
+            fig, ax = plt.subplots()
+            ax.plot(d[i][0], F[i][0], 'deepskyblue', label='force-distance curve')
+            ax.plot(x, lin_fit, 'orange', label='linear fit line')
+            ax.plot(d[i][0][:percentage_slice], F[i][0][:percentage_slice], 'red', label='part of curve used in the linear fit')
+            ax.set(xlabel='d', ylabel='force', title='Force-distance curve %i' % i)
+            plt.legend(loc="upper right")
+            if saveplot == 'True':
+                fig.savefig('Results\Fd_baseline_linearfit_' + str(i) + '.png')
+    
+    return M, B
+
+def contactPoint(F, d, plot='False', saveplot='False'):
+    F_bS = baselineSubtraction(F)
+    M, B = baselineLinearFit(F_bS, d)
+    # empty list to store the index of the last intersection point of the F-d curve with the linear fit line 
+    argmin_list = []
+    for i in range(len(F_bS)):
+        difference_list = []
+        for j in range(len(F_bS[i][0])):
+            f = M[i]*(d[i][0][j]) + B[i] # linear fit line
+            difference_squared = (F_bS[i][0][j] - f)**2 # the difference-swuared between the force value and the value of the linear fit line at each point
+            difference_list.append(difference_squared)
+
+        argmin_val = [i for i,el in enumerate(difference_list) if abs(el) < 0.001][-1] # index of the last intersection point of the F-d curve with the linear fit line 
+        argmin_list.append(argmin_val)
+
+        if plot == 'True':
+            fig, ax = plt.subplots()
+            ax.plot(d[i][0], F_bS[i][0], 'deepskyblue', label='force-distance curve')
+            ax.plot(d[i][0], M[i]*(d[i][0]) + B[i], 'orange', label='linear fit line')
+            ax.plot(d[i][0][argmin_val], F_bS[i][0][argmin_val], 'ro', label='contact point estimation')
+            ax.set(xlabel='z', ylabel='force', title='Force-distance curve %i' % i)
+            plt.legend(loc="upper right")
+            if saveplot == 'True':
+                fig.savefig('Results\Fd_contact_point_' + str(i) + '.png')
+    
+    return argmin_list
